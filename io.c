1a4cdb2fefc75a4b1c4b26a44ba65782a63821fa
peter <peter@cordes.ca>
2007-01-24 14:54:52 -0400

    make the io routines use procov's struct noeud to get the name field
    darcs-hash:20070124185452-0feb0-09c521de108fc04e24069f3e7f4feae1b82f1b1e

--- io.c
+++ io.c
@@ -8,7 +8,13 @@
 #include <string.h>
 #include <assert.h>
 
-#define SPR_NODE_DATAPTR_TYPE struct spr_nodename
+#define nom name
+#include "../procov/mynhmlg.h"
+
+//#define SPR_NODE_DATAPTR_TYPE struct spr_nodename
+#define SPR_NODE_DATAPTR_TYPE struct noeud
+//#define name nom
+
 #include "spr.h"
 #include "spr_private.h"
 
