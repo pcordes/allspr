0428036550bad2d1741afa4c1fcc7ae396d391c5
peter <peter@cordes.ca>
2007-03-24 05:40:18 -0300

    give brontler useful command line options, and update to work after the massive cleanup library patch
    includes debug-level options, and ability to read a tree from a file
    
    darcs-hash:20070324084018-0feb0-5db8b0edeb1116e1376e4c7d2ff88f81468da033

--- brontler.c
+++ brontler.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 #include <assert.h>
 
 
@@ -24,27 +25,44 @@ struct nodedata{
  * that we will actually store in it, instead of a void *
  * so we don't have to cast all the time */
 //#define SPR_NODE_DATAPTR_TYPE struct nodedata
+// actually just use whatever the library was compiled with
+// procov's data->name field is an array in the struct, not a char *.
+// this necessitates some alternate code (strcpy instead of strdup)... see the #ifs below
 #define SPR_PRIVATE
 #include <spr.h>
 
+// globals
+int debug = 0;
+
+const char *usage=
+"usage: brontler [options] tree [src dest]\n"
+" brontler '(((a,b),(d,(e,f))),(c,d))' dump all unique SPRs\n"
+" brontler '(a,(c,b))' a c\t- SPR from a to c.  (internal nodes have capital letter names...)\n"
+"options: -h, -V: help and version\n"
+"\t-t tree\tread tree from a file instead of the command line\n"
+"\t-d number\tdebug level (default 0)\n"
+"\t-D number\tallspr library debug level (default 0)\n"
+"\t-m mode\t0: just exhaust SPRs from the starting tree. (default)\n"
+"\t  1: exhaust SPRs from the starting tree, then start from the last SPR.\n"
+"\t  2: take the SPRed topology as a new start point 50% of the time.\n"
+"\tboth non-zero modes only stop when no non-duplicate SPRs can be done.\n";
+
+const char *version="brontler v2.0. allspr library version " ALLSPR_VERSION "\n";
+
+
 
 /* a "constructor".  returns a malloc()ed spr_node */
-struct spr_node *newnode(char *name)
+static struct spr_node *newnode(char *name)
 {
-	struct spr_node *p = xcalloc(1, sizeof(*p));
-//	struct nodedata *d = xcalloc(1, sizeof(*d));
-	typeof(p->data) d = xcalloc(1, sizeof(*d));
-
-	p->left=p->right=p->parent=NULL;
-	p->data = d;
-
+	SPR_NODE_DATAPTR_TYPE *d = xcalloc(1, sizeof(*d));
+#ifdef SPR_PROCOV_DATA
 	if (name) strcpy (d->name, name);
-//	d->name=name;
-//	d->dirty=0;
-//	d->bl=0;
-//	d->dna=NULL;
+#else
+	d->name=name;
+	d->dirty=0; d->bl=0; d->dna=NULL;
+#endif // procov
 
-	return p;
+	return spr_newnode( NULL, NULL, NULL, d);
 }
 
 
@@ -67,12 +85,15 @@ struct spr_node *parsenewick( char *str, int *len )
 	static char nextname[] = { 'A', '\0' };
 	int tmp;
 //	char *name;
-	struct spr_node *node = newnode( NULL );
-	typeof (node->data) data = node->data;
+	struct spr_node *node = newnode(NULL);
+	SPR_NODE_DATAPTR_TYPE *data = node->data;
 
 	if (*str == '('){  // internal node
-//		data->name = strdup(nextname);
+#ifdef SPR_PROCOV_DATA
 		strcpy (data->name, nextname);
+#else
+		data->name = strdup(nextname);
+#endif // procov
 		++*nextname;
 
 		node->left = parsenewick(str+1, &tmp);
@@ -98,8 +119,11 @@ struct spr_node *parsenewick( char *str, int *len )
 	}else if (strspn(str, " -_.abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")){
 		// leaf node
 		tmp = 0;  // let scanf handle taxon names
-//		if ( 1 > sscanf( str, " %a[^][)(:;, \f\n\r\t\v] %n", &data->name, len))
+#ifdef SPR_PROCOV_DATA
 		if ( 1 > sscanf( str, " %[^][)(:;, \f\n\r\t\v] %n", data->name, len))
+#else
+		if ( 1 > sscanf( str, " %a[^][)(:;, \f\n\r\t\v] %n", &data->name, len))
+#endif // procov
 			fprintf(stderr, "bad newick element: \"%s\"\n",str);
 	}else{ // not a node!?
 		fprintf(stderr, "bad newick element, not an open paren or a taxon name: \"%s\"\n",str);
@@ -107,43 +131,38 @@ struct spr_node *parsenewick( char *str, int *len )
 		return NULL;
 	}
 
-	float dummy;
 	// internal and leaf nodes can have branch lengths
-//	if( sscanf(str+*len, " : %f %n", &data->bl, &tmp) )
-	if( sscanf(str+*len, " : %f %n", &dummy, &tmp) )
+#ifdef SPR_PROCOV_DATA
+	float dummy;
+	if (sscanf(str+*len, " : %f %n", &dummy, &tmp))
+#else
+	if (sscanf(str+*len, " : %f %n", &data->bl, &tmp))
+#endif // procov
 		*len += tmp;
 
 	return node;
 }
 
 /* build up a little tree by hand for testing */
-void sprtest(void)
+static void sprtest(void)
 {
 	struct spr_node *root;
 	struct spr_tree *libstate;
 #define decnode(x) struct spr_node *x = newnode(#x)
-	decnode(A);
-	decnode(B);
-	decnode(C);
-	decnode(D);
-	decnode(E);
-	decnode(F);
-	decnode(G);
+	decnode(A);	decnode(B);	decnode(C);	decnode(D);
+	decnode(E);	decnode(F);	decnode(G);
 // do both directions
 #define link(p,rel,c) p->rel = c; c->parent=p;
-	link(A,left,B);
-	link(A,right,D);
+	link(A,left,B);	link(A,right,D);
 
-	link(B,left,C);
-	link(B,right,G);
-	link(D,left,E);
-	link(D,right,F);
+	link(B,left,C);	link(B,right,G);
+	link(D,left,E);	link(D,right,F);
 	// constructor inits other pointers to NULL.
 
-	newickprint(A);
+	newickprint(A, stdout);
 	puts("\n");
-	newickprint(D);
-	treeprint(D);
+	newickprint(D, stdout);
+	treeprint(D, stderr);
 	
 	if (NULL == (libstate = spr_init( A, NULL, 0 ))){
 		fputs("couldn't init libspr\n", stderr );
@@ -151,77 +170,130 @@ void sprtest(void)
 	}
 //	spr_libsprtest( libstate );
 
-	spr( libstate, C, F );
-	newickprint( A );
+	spr(libstate, C, F);
+	newickprint(A, stdout);
 	root = spr_findroot(A);
-	newickprint( root );
-	spr_statefree( libstate );
-	spr_treefree( root, TRUE );
+	newickprint(root, stdout);
+	spr_statefree(libstate);
+	spr_treefree(root, TRUE);
 }
-char *usage=
-"usage: brontler '(((a,b),(d,(e,f))),(c,d))' - do a fixed SPR\n"
-"brontler '(a,(c,b))' foo\t- dump all unique SPR\n"
-"brontler '(a,(c,b))' a c\t- SPR from a to c.  (internal nodes have capital letter names...)\n"
-"debugging output is always printed before doing anything\n";
+
+// return a malloc()ed buffer holding the entire contents of the file, nul terminated.
+char *readfile(char *file)
+{
+	int offset = 0, size = 4000, tmp;
+	char *buf = xmalloc(size);
+	FILE *f = fopen(file, "r");
+	if (!f){ perror("brontler: error opening tree file:" ); exit(2); }
+	while ((tmp=fread(buf+offset, 1, size, f))  > 0){
+		offset += tmp;
+		size*=2;
+		buf=xrealloc(buf, size*2); // yes, 4 times as big as first iteration
+		// after the first time through the loop, read goes from 1/4 to 3/4 of buf
+	}
+	if (!feof(f) || ferror(f) || fclose(f)){ perror("brontler: error reading tree file:" ); exit(2); }
+	buf[offset] = '\0';
+	buf = xrealloc(buf, strlen(buf)+1);
+	return buf;
+}
+
+// This is where the action is:
+// enumerate the possible SPRs, one per line with various counters.
+// see usage string for meaning of mode.
+static int allspr(struct spr_tree *sprtree, int spr_mode)
+{
+	int treeiter, bestspr, treecount, sprnum;
+	printf ("tree: taxa: %d, nodes: %d, possible SPRs <= %d\n",
+		sprtree->taxa, sprtree->nodes, sprtree->lcg.m );
+	// tree->lcg.state = 16;
+
+	for(treecount=0, treeiter=1 ; ; treeiter++){
+		bestspr = 0;
+		while ( (sprnum = spr_next_spr(sprtree)) ){
+			if (debug>=2) treeprint(sprtree->root, stderr);
+			printf("%d: tree %d.%d: ", ++treecount, treeiter, sprnum);
+			newickprint(sprtree->root, stdout);
+			bestspr = sprnum;
+			if (spr_mode==2 && rand()%2) break;
+		}
+		if(spr_mode > 0 && bestspr) spr_apply_sprnum(sprtree, bestspr);
+		else break;
+	}
+	return TRUE;
+}
 
 int main (int argc, char *argv[])
 {
-	struct spr_tree *tree;
-	struct spr_node *root;
-
-	int i, tmp;
+	struct spr_tree *sprtree;
+	struct spr_node *root, *src, *dest;
+	char *treestring = NULL;
+	int spr_mode=0, i, tmp;
 	
 //	srand( time(NULL) );
 	srand( 42 );
 
-	// sprtest();
-
-	if(argc<2){
-		fputs(usage,stderr);
-		return 1;
+	opterr = 1; // make getopt print specific error messages for us
+	while ((i = getopt (argc, argv, "hVD:d:m:t:")) != -1){
+	  switch(i){
+	  case 'h': puts(usage);   return 0;
+	  case 'V': puts(version); return 0;
+	  case 'd': debug=atoi(optarg); break;
+	  case 'D': spr_setdebug(atoi(optarg)); break;
+	  case 'm': spr_mode=atoi(optarg); break;
+	  case 't': treestring=readfile(optarg); break;
+	  case '?':
+		  fputs("you need -h (help)\n", stderr);
+		  return 1;
+	  default:
+		  abort ();	// this should never happen
+	  }
 	}
-	// parse tree from cmdline and print it out
-	root = parsenewick( argv[1], &tmp ); // assert (tmp == strlen)...
-	assert( root == spr_findroot(root) );
-	treeprint( root );
-	newickprint( root );
 
-	if (NULL == (tree = spr_init( root, NULL, FALSE ))){
-		fputs("couldn't init libspr\n", stderr );
+	if (42 == debug) sprtest();
+
+	if (!treestring){ // take the tree from the command line
+		if ((argc - optind) < 1){
+			fputs("brontler: error: no tree specified\n", stderr);
+			fputs(usage,stderr);
+			return 1;
+		}else
+			treestring = argv[optind++];
+	}
+
+	// parse tree and print it out
+	root = parsenewick(treestring, &tmp); // assert (tmp == strlen)...
+	assert( root == spr_findroot(root) );
+	if (debug>=1){
+		puts("starting tree:");
+		treeprint(root, stderr);
+		newickprint(root, stdout);
+	}
+
+	if (NULL == (sprtree = spr_init(root, NULL, FALSE))){
+		fputs("couldn't init libspr\n", stderr);
 		return 2;
 	}
 
-
-	if (argc != 3){
-		puts("doing SPR...");
-		if( argc < 2 ){
-			tmp = spr( tree, root->left->left, root->right->left );
-		}else if (argc == 4){
-			tmp = spr( tree, spr_searchbyname(root, argv[2]), 
-			     spr_searchbyname(root, argv[3]));
-			// This is lame because internal node names are weird
+	switch (argc - optind){
+	case 0: allspr(sprtree, spr_mode); break;
+	case 2:
+		src  = spr_treesearchbyname(sprtree, argv[argc-optind]);
+		dest = spr_treesearchbyname(sprtree, argv[argc-optind+1]);
+		if (debug>=2) puts("doing SPR...");
+		spr(sprtree, src, dest);
+		if (debug>=1){
+			puts(tmp ? "SPR succeeded" : "SPR failed");
+			treeprint(sprtree->root, stderr);
 		}
-		// newickprint( root );
-		puts(tmp ? "SPR succeeded" : "SPR failed");
-		treeprint( tree->root );
-		newickprint( tree->root );
+		newickprint(sprtree->root, stdout);
+		break;
+	default:
+		fputs("wrong number of non-option arguments. you need -h (help)\n", stderr);
+		return 1;
 	}
 
-	if (argc == 3){
-		i=0;
-		printf ("tree: taxa: %d, nodes: %d, possible SPRs <= %d\n",
-			tree->taxa, tree->nodes, tree->lcg.m );
-		//		tree->lcg.startstate=tree->lcg.state=16;
-		lcg(&tree->lcg);
-		while ( (tmp = spr_next_spr(tree)) ){
-//			treeprint( tree->root );
-			printf("%d: tree %d: ", ++i, tmp );
-			newickprint( tree->root );
-		}
-	}
-
-	spr_statefree( tree );
-	spr_treefree( root, TRUE );
+	spr_statefree(sprtree);
+	spr_treefree(root, TRUE);
 	spr_staticfree();
 	return 0;
 }
